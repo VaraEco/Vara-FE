85d94316e8ce09c0b04b1c8795b7cba178107f9a
Shraddha  Agrawal <shraddhaagrawal@Shraddhas-MacBook-Pro.local>
2024-07-19 16:18:17 -0700

    setup env variables

--- src/assets/Config/s3Config.jsx
+++ src/assets/Config/s3Config.jsx
@@ -5,28 +5,11 @@ const S3_BUCKET = "compliance-document-bucket"; // Replace with your bucket name
 const REGION = "us-east-1"; // Replace with your region
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
 });
 
 export const s3 = new S3({
   params: { Bucket: S3_BUCKET },
   region: REGION,
 });
-
-
-// import { createClient } from "@supabase/supabase-js";
-// import { mainConfig } from "./assets/Config/appConfig";
-
-// const isLocal = window.location.hostname === 'localhost';
-
-// if (isLocal) {
-//     console.log('Running locally');
-// } else {
-//     console.log('Running on the cloud');
-// }
-
-// const supabaseUrl = mainConfig.REACT_APP_SUPABASE_URL;
-// const supabaseKey = mainConfig.REACT_APP_SUPABASE_ANON_KEY;
-
-// export const supabase = createClient(supabaseUrl, supabaseKey);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+      define: {
+        'process.env.REACT_APP_AWS_ACCESS_KEY_ID': JSON.stringify(env.REACT_APP_AWS_ACCESS_KEY_ID),
+        'process.env.REACT_APP_AWS_SECRET_ACCESS_KEY': JSON.stringify(env.REACT_APP_AWS_SECRET_ACCESS_KEY)
+      },
+      plugins: [react()],
+      server: {
         port: 3000,
-    },
-});
+      }
+    }
+  })

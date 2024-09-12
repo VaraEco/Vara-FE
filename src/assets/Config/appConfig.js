a7c493595d5d20b529fe7a2b508126c8e89fefa3
soumitra-quest <soumitra.d@questlabs.biz>
2024-09-12 16:33:41 +0530

    update package for organization email support

--- src/assets/Config/appConfig.js
+++ src/assets/Config/appConfig.js
@@ -1,5 +1,5 @@
 export const mainConfig = {
-    QUEST_BASE_URL : "https://api.questprotocol.xyz/",
+    QUEST_BASE_URL : "https://api.questlabs.ai/",
     QUEST_ENTITY_ID: "e-c05832ae-0a36-4907-adcc-601084649fba",
     QUEST_API_KEY: "k-758f7d33-6133-4e52-ae85-86bf1484ddd1",
     CALENDLY_LINK: "https://calendly.com/",
